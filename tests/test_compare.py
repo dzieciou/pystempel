95d99097ba7ef129f2327c8846c04d90d5f4f6eb
Maciej Gawinecki <mgawinecki@gmail.com>
2021-03-13 12:16:20 +0100

    Refactor locating data files

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -15,16 +15,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import os
-
 import pytest
 
-import tests.base as base
+from tests import base
+from tests.base import get_test_data_path, get_stempel_jar_path, get_library_data_path
 
-pwd = os.path.dirname(os.path.abspath(__file__))
-stemmer_table_fpath = os.path.join(pwd, "..", "data", "original", "stemmer_20000.tbl")
-jar_fpath = os.path.join(os.getcwd(), "stempel-8.1.1.jar")
-dict_fpath = os.path.join(pwd, "sjp_dict.txt")
+stemmer_table_fpath = get_library_data_path("original", "stemmer_20000.tbl")
+jar_fpath = get_stempel_jar_path()
+dict_fpath = get_test_data_path("sjp_dict.txt")
 
 python_stemmer = base.get_python_stemmer(stemmer_table_fpath)
 java_stemmer = base.get_java_stemmer(stemmer_table_fpath, jar_fpath)
@@ -35,5 +33,5 @@ def test_stemming(word):
     python_stem = python_stemmer.stem(word)
     java_stem = java_stemmer.stem(word)
     assert (
-        python_stem is None and java_stem is None
-    ) or python_stem == java_stem.toString()
+                   python_stem is None and java_stem is None
+           ) or python_stem == java_stem.toString()
